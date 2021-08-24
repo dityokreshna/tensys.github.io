9952b472058de24a07cef26e61dfde37c27f5c9d
Dityo Kreshna <dityo.kreshna354@gmail.com>
2021-08-25 06:16:45 +0700

    revisi lagi
    revisi lagi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0ba75b9a236673cb825a7e7229616c23",
-"/": "0ba75b9a236673cb825a7e7229616c23",
-"main.dart.js": "bc6133619c654d742b095161cb9978a0",
+"index.html": "09369ea06dee7249e854e3bf7b3f974c",
+"/": "09369ea06dee7249e854e3bf7b3f974c",
+"main.dart.js": "c42cc90632ab78e0af137274de27e8aa",
 "manifest.json": "5c4b1ffce058de1d636e78dd4259e633",
 "version.json": "bd7a02885ccaff5482d629fdadef6f5b"
 };
